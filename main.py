25084ba8a26719753556a502fc4b3508ad478a81
juancarlos923 <82616348+juancarlos923@users.noreply.github.com>
2021-06-02 21:43:14 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import urllib.request, json
+
+cep = input("Informe um CEP:")
+
+with urllib.request.urlopen(f"https://viacep.com.br/ws/{cep}/json/") as url:
+    dados = json.loads(url.read().decode())
+
+print('Estado: ' + dados['uf'])
+
+if (dados['bairro'] != ""):
+    print('Bairro: ' + dados['bairro'])
+
+print('Cidade: ' + dados['localidade'])
+
+if (dados['logradouro'] != ""):
+    print('Rua: ' + dados['logradouro'])
+
+print('DDD: ' + dados['ddd'])
+
+#Nem Sempre o CEP é de uma rua e bairro específico.
